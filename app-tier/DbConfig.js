9b0f0b731b64b28a2aef1b457dd32b1ed15d78a5
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-05-13 12:26:02 +0530

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cmtc2kgsyrct.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'P#Fs4]4p#|ZwF#Zb]ZU6P2pyKw]5',
+    DB_DATABASE : 'webappdb'
+});
